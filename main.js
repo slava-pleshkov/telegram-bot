50b59e40d5226e3b9560f79826b8b9f2a963249a
Slava Pleshkov <mail@slavapleshkov.com>
2019-02-11 17:28:31 +0200

    aAdd file

--- main.js
+++ main.js
@@ -1,10 +1,26 @@
 require('dotenv').config()
 const Telegraf = require('telegraf')
+const express = require('express');
+const expressApp = express();
 
-const bot = new Telegraf(process.env.API_TOKEN)
+const API_TOKEN = process.env.API_TOKEN || '';
+const PORT = process.env.PORT || 3000;
+const URL = process.env.URL || 'https://your-heroku-app.herokuapp.com';
+
+const bot = new Telegraf(API_TOKEN);
+bot.telegram.setWebhook(`${URL}/bot${API_TOKEN}`);
+expressApp.use(bot.webhookCallback(`/bot${API_TOKEN}`));
+
 bot.start((ctx) => ctx.reply('Welcome!'))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.hears('hi', (ctx) => ctx.reply('Hello'))
 bot.hears('author', (ctx) => ctx.reply('Slava Pleshkov <mail@slavapleshkov.com>'))
 bot.hears('mysite', (ctx) => ctx.reply('https://slavapleshkov.com/'))
 bot.launch()
+
+expressApp.get('/', (req, res) => {
+    res.send('@SlavaPleshkovBot');
+  });
+  expressApp.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
